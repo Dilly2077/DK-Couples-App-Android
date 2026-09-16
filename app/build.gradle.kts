45f8b95c14f0a97a3960abd167c5ae61032d7072
Dilly2077 <khaznadar.dlovan@gmail.com>
2026-09-16 20:09:55 +0100

    Ensure Evelune UI icon coverage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.compose.material:material-icons-extended")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     testImplementation("junit:junit:4.13.2")
