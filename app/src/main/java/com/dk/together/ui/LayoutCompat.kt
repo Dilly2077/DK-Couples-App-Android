25116e002ec89f442a7c666045dd80f14c49f5b8
Dilly2077 <khaznadar.dlovan@gmail.com>
2026-09-16 23:31:33 +0100

    Fix width constraint helper import

--- app/src/main/java/com/dk/together/ui/LayoutCompat.kt
+++ app/src/main/java/com/dk/together/ui/LayoutCompat.kt
@@ -1,7 +1,8 @@
 package com.dk.together.ui
 
+import androidx.compose.foundation.layout.widthIn as composeWidthIn
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 
 fun Modifier.widthIn(min: Dp = Dp.Unspecified, max: Dp = Dp.Unspecified): Modifier =
-    androidx.compose.foundation.layout.widthIn(this, min = min, max = max)
+    this.composeWidthIn(min = min, max = max)
