a141036334bd1aa7727db92f4fd778a6b07f7f94
Dilly2077 <khaznadar.dlovan@gmail.com>
2026-09-16 23:34:27 +0100

    Remove temporary layout helper

--- app/src/main/java/com/dk/together/ui/LayoutCompat.kt
+++
@@ -1,8 +0,0 @@
-package com.dk.together.ui
-
-import androidx.compose.foundation.layout.widthIn as composeWidthIn
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
-
-fun Modifier.widthIn(min: Dp = Dp.Unspecified, max: Dp = Dp.Unspecified): Modifier =
-    this.composeWidthIn(min = min, max = max)
